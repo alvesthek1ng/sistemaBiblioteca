d52eef173f3e2f9dd2e4c1811611d147ae7abede
alvesthek1ng <netococ6@gmail.com>
2024-10-19 15:29:10 -0300

    Criei os atributos e o método construtor da Classe Cliente

--- mavenproject1/src/main/java/sistemaBiblioteca/Cliente.java
+++ mavenproject1/src/main/java/sistemaBiblioteca/Cliente.java
@@ -1,5 +1,16 @@
 package sistemaBiblioteca;
 
 public class Cliente {
-    
+    //Atributos de cliente todos privados
+
+    private String nome;
+    private String email;
+    private int id;
+
+    // Método construtor da classe cliente
+    public Cliente(String nome, String email, int id) {
+        this.nome = nome;
+        this.email = email;
+        this.id = id;
+    }
 }
