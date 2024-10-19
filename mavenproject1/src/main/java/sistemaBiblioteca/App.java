ce266f2bfb9028bbf8901664323c85a9f1473e87
DavidCrr <david.contato62@gmail.com>
2024-10-19 01:57:07 -0300

    implementações da classe app
    importei a classe Scanner e criei os arrays para salvar os dados de livros, usuários e empréstimos.

--- mavenproject1/src/main/java/sistemaBiblioteca/App.java
+++ mavenproject1/src/main/java/sistemaBiblioteca/App.java
@@ -1,5 +1,9 @@
 package sistemaBiblioteca;
+import java.util.Scanner;
 
 public class App {
-    
+    // arrays para armazenar livros,usuarios e emprestimos
+    private static livro[] livros;
+    private static usuario[] usuarios;
+    private static emprestimo[] emprestimos;
 }
