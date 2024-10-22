4f1df6b7db715bbf170d916ed43b203924a546c9
alvesthek1ng <netococ6@gmail.com>
2024-10-22 19:46:54 -0300

    criei os metodos Get e Set

--- mavenproject1/src/main/java/sistemaBiblioteca/Cliente.java
+++ mavenproject1/src/main/java/sistemaBiblioteca/Cliente.java
@@ -2,15 +2,24 @@ package sistemaBiblioteca;
 
 public class Cliente {
     //Atributos de cliente todos privados
-
     private String nome;
     private String email;
     private int id;
+    private String telefone;
 
     // Método construtor da classe cliente
     public Cliente(String nome, String email, int id) {
         this.nome = nome;
         this.email = email;
         this.id = id;
+        this.telefone = telefone;
+    }
+    
+    public String getNome(){
+        return nome;
+    }
+    
+    public String setNome(){
+        return nome;
     }
 }
