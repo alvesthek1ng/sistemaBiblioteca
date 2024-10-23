c743b4a54fde2219922c6ff06e5e128323596862
TheusZ21 <victormatheusribeiro2014@outlook.com>
2024-10-23 17:18:21 -0300

    criei as variaveis e comecei o construtor

--- mavenproject1/src/main/java/sistemaBiblioteca/Livro.java
+++ mavenproject1/src/main/java/sistemaBiblioteca/Livro.java
@@ -1,5 +1,22 @@
 package sistemaBiblioteca;
 
 public class Livro {
+    //criando as variaveis
+    String titulo;
+    String autor;       
+    float AnoDePublicacao;
+    float NumeroDeExemplaresDisponiveis;
+    
+    
+    //construtor
+    private (STring titulo, String autor, Float AnoDePublicacao, Float NumeroDeExemplaresDisponiveis);
+    this.titulo;
+
+   
+    }
+.
+    
+    
+    
     
 }
