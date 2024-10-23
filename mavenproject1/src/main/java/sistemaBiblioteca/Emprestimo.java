ff8ee29c09abd47864e987b66e5a0451463ac556
igorlhorrann <igor.lhorrann@estudante.ifgoiano.edu.br>
2024-10-22 21:34:04 -0300

    colocando titulo e autor obrigatorio

--- mavenproject1/src/main/java/sistemaBiblioteca/Emprestimo.java
+++ mavenproject1/src/main/java/sistemaBiblioteca/Emprestimo.java
@@ -1,5 +1,17 @@
 package sistemaBiblioteca;
 
 public class Emprestimo {
+    private String titulo;
+    private String autor;
     
-}
+    public Emprestimo(String titulo, String autor) {
+        this.titulo = titulo;
+        this.autor = autor;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+  
+  
+}  
