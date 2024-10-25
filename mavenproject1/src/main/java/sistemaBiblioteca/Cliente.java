378d5d55d6f16fb1b0b82280471b35bc11969a34
alvesthek1ng <netococ6@gmail.com>
2024-10-25 20:32:27 -0300

    atualizei a classe Cliente para verificar se email e telefone são validos

--- mavenproject1/src/main/java/sistemaBiblioteca/Cliente.java
+++ mavenproject1/src/main/java/sistemaBiblioteca/Cliente.java
@@ -1,6 +1,7 @@
 package sistemaBiblioteca;
 
 public class Cliente {
+
     //Atributos de cliente todos privados
     private String nome;
     private String email;
@@ -14,25 +15,48 @@ public class Cliente {
         this.id = id;
         this.telefone = telefone;
     }
+
     // Criei os metodos get
-    public String getNome(){
+    public String getNome() {
         return nome;
     }
-    
-    public String getEmail(){
+
+    public String getEmail() {
         return email;
     }
-    
-    public String getTelefone(){
+
+    public String getTelefone() {
         return telefone;
     }
-    
+
     // Criei os metodos set
     public void setEmail(String email) {
+        if(!email.contains("@")){
+            throw new IllegalArgumentException("Email inválido");
+        }
         this.email = email;
     }
 
     public void setTelefone(String telefone) {
+        if(!telefone.matches("\\d{10,15}")){
+            throw new IllegalArgumentException("Telefone inválido");
+        }
         this.telefone = telefone;
     }
+
+    
+    
+    
+    
+    
+    @Override
+    public String toString() {
+        return "Cliente{"
+                + "nome='" + nome + '\''
+                + ", email='" + email + '\''
+                + ", id=" + id
+                + ", telefone='" + telefone + '\''
+                + '}';
+    }
+
 }
