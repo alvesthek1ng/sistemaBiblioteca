c2d67cc3a4e387393b95bbb4ce631663452f4c3c
DavidCrr <david.contato62@gmail.com>
2024-10-25 22:14:17 -0300

    correçao dos nomes de classes

--- mavenproject1/src/main/java/sistemaBiblioteca/App.java
+++ mavenproject1/src/main/java/sistemaBiblioteca/App.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class App {
     // arrays para armazenar livros,usuarios e emprestimos
-    private static livro[] livros;
-    private static usuario[] usuarios;
-    private static emprestimo[] emprestimos;
+    private static Livro[] livros;
+    private static Cliente[] Clintes;
+    private static Emprestimo[] emprestimos;
 }
