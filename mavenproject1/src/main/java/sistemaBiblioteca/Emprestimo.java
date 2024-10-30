041d53ff31023849c7fd8ad3ca7bba7e2e12d29a
DavidCrr <david.contato62@gmail.com>
2024-10-30 18:18:43 -0300

    implementei os metodos get eo atributo que faltava
    implentei o atributo "ativo" para verificar se o emprestimo esta a ativo e criei metodos get para o id , livro e cliente dentro da classe emprestimo alem do set id e setativo.

--- mavenproject1/src/main/java/sistemaBiblioteca/Emprestimo.java
+++ mavenproject1/src/main/java/sistemaBiblioteca/Emprestimo.java
@@ -5,24 +5,43 @@ public class Emprestimo {
     private String dataEmprestimo;
     private Cliente cliente; 
     private Livro livro;
+    private int id; 
+    private boolean ativo;
     
     //metodo construtor 
-    public Emprestimo(String dataDevolucao, String dataEmprestimo, Cliente cliente, Livro livro) {
+    public Emprestimo(String dataDevolucao, String dataEmprestimo, Cliente cliente, Livro livro, boolean ativo) {
         this.dataEmprestimo = dataEmprestimo;
         this.dataDevolucao = dataDevolucao;
         this.cliente = cliente;
         this.livro = livro;
+        this.ativo = true;
     }
     
     //metodo get
+    
+    public int getId(){
+        return id;
+    }
+    public Livro getLivro(){
+        return livro;
+    }
+    public Cliente getCliente(){
+        return cliente;
+    }
+    
     public String getDataEmprestimo(){
         return dataEmprestimo;
     }
-  
+      public boolean Ativo() {
+        return ativo;
+    }
     //metodo set
     public String setDataEmprestimo(){
         this.dataEmprestimo = dataEmprestimo;
         return null;
+    }
+    public void setId(int id){
+        this.id = id;
     }
     
     
@@ -34,7 +53,10 @@ public class Emprestimo {
          this.dataDevolucao = dataDevolucao;    
         return null;
     }
-    
+        public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+
      
     @Override
     public String toString(){
